ca564b04848fc758e2bfc3b244cf6224e5a6779c
Your Name <liemhoanglong@gmail.com>
2019-11-03 09:42:58 +0700

    done

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,9 +5,40 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
 /* GET shop page. */
 router.get('/shop', function(req, res, next) {
   res.render('shop');
 });
 
+/* GET shop page. */
+router.get('/cart', function(req, res, next) {
+  res.render('cart');
+});
+
+/* GET shop page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET shop page. */
+router.get('/my-account', function(req, res, next) {
+  res.render('my-account');
+});
+
+/* GET checkout page. */
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout');
+});
+
+/* GET product-detail page. */
+router.get('/product-details', function(req, res, next) {
+  res.render('product-details');
+});
+
+
+/* GET forget-pass page. */
+router.get('/forget-pass', function(req, res, next) {
+  res.render('forget-pass');
+});
 module.exports = router;
